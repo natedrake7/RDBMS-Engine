c9f68aedef626bc0ad1c2bef4a91711a0739b656
natedrake7 <kglampro1@gmail.com>
2025-01-22 17:16:37 +0200

    Added more aggregate functions declarations

--- Database/AggregateFunctions/AggregateFunctions.h
+++ Database/AggregateFunctions/AggregateFunctions.h
@@ -17,5 +17,8 @@ class AggregateFunctions {
     public:
         static double Average(const vector<DatabaseEngine::StorageTypes::Row>& rows, const Constants::column_index_t& columnIndex, const double* constantValue = nullptr);
         static uint64_t Count(const vector<DatabaseEngine::StorageTypes::Row>& rows, const Constants::column_index_t& columnIndex, const double* constantValue = nullptr);
+        static double Max(const vector<DatabaseEngine::StorageTypes::Row>& rows, const Constants::column_index_t& columnIndex, const bool& isSelectedColumnIndexed = false, const double* constantValue = nullptr);
+        static double Min(const vector<DatabaseEngine::StorageTypes::Row>& rows, const Constants::column_index_t& columnIndex, const bool& isSelectedColumnIndexed = false, const double* constantValue = nullptr);
+        static double Sum(const vector<DatabaseEngine::StorageTypes::Row>& rows, const Constants::column_index_t& columnIndex, const double* constantValue = nullptr);
         
 };
