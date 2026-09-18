0bf0c9c40feb6fa5cba5a65a835d37636cda74a1
Lampropoulos Konstantinos <lamproko@crediabank.com>
2026-09-18 14:15:48 +0300

    added include in wire types.

--- src/Systemic/include/Network/WireTypes.h
+++ src/Systemic/include/Network/WireTypes.h
@@ -2,6 +2,7 @@
 #include "../DataTypes/DateTime.h"
 #include "../DataTypes/Guid.h"
 #include "../DataTypes/DataTypes.h"
+#include "DataTypes/PackedWord.h"
 
 namespace Network{
     inline constexpr UnsignedSmallInt PROTOCOL_VERSION = 1;
